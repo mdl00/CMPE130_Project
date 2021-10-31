6797a3e0ee6a66b8f2e8c10174e457ddbbeb08f1
mdl00 <mdleeper@gmail.com>
2021-10-30 21:19:40 -0700

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -1,24 +1,109 @@
-message = int(input("Enter the message to be encrypted: ")) 
+import random
+
+# Pre generated primes
+first_primes_list = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
+                     31, 37, 41, 43, 47, 53, 59, 61, 67,
+                     71, 73, 79, 83, 89, 97, 101, 103,
+                     107, 109, 113, 127, 131, 137, 139,
+                     149, 151, 157, 163, 167, 173, 179,
+                     181, 191, 193, 197, 199, 211, 223,
+                     227, 229, 233, 239, 241, 251, 257,
+                     263, 269, 271, 277, 281, 283, 293,
+                     307, 311, 313, 317, 331, 337, 347, 349]
  
-p = 11
-q = 13
-e = 7
-n = p*q
-z = (p-1)*(q-1)
+def nBitRandom(n):
+    return random.randrange(2**(n-1)+1, 2**n - 1)
  
-def encrypt(m):
+def getLowLevelPrime(n):
+    '''Generate a prime candidate divisible
+    by first primes'''
+    while True:
+        # Obtain a random number
+        pc = nBitRandom(n)
+ 
+         # Test divisibility by pre-generated
+         # primes
+        for divisor in first_primes_list:
+            if pc % divisor == 0 and divisor**2 <= pc:
+                break
+        else: return pc
+ 
+def isMillerRabinPassed(mrc):
+    '''Run 20 iterations of Rabin Miller Primality test'''
+    maxDivisionsByTwo = 0
+    ec = mrc-1
+    while ec % 2 == 0:
+        ec >>= 1
+        maxDivisionsByTwo += 1
+    assert(2**maxDivisionsByTwo * ec == mrc-1)
+ 
+    def trialComposite(round_tester):
+        if pow(round_tester, ec, mrc) == 1:
+            return False
+        for i in range(maxDivisionsByTwo):
+            if pow(round_tester, 2**i * ec, mrc) == mrc-1:
+                return False
+        return True
+ 
+    # Set number of trials here
+    numberOfRabinTrials = 20
+    for i in range(numberOfRabinTrials):
+        round_tester = random.randrange(2, mrc)
+        if trialComposite(round_tester):
+            return False
+    return True
+ 
+def generatePrime(n):
+ while True:
+     prime_candidate = getLowLevelPrime(n)
+     if not isMillerRabinPassed(prime_candidate):
+         continue
+     else:
+         print(n, "bit prime is: \n", prime_candidate)
+         break
+
+def generatePubKey(p, q):
+    n = p*q
+    z = (p-1)*(q-1)
+    e = nBitRandom(2048)
+    while (e >= n or gcd(e, z) != 1)
+         e = nBitRandom(2048)
+    return e
+ 
+def calculatePrivKey(p, q, e):
+    n = p*q
+    z = (p-1)*(q-1)
+    d = nBitRandom(2048)
+    while (int((d*e) % z) is not 1 or d >= n):
+        d = nBitRandom(2048)
+    return d
+ 
+def encrypt(m, e):
     c = int((m**e) % n)
     return c
 
-def decrypt(c):
-    d = 1
-    while (int((d*e) % z) is not 1 and d < 100000):
-        d = d + 1
+def decrypt(c, d):
     m = int((c**d) % n)
     return m
- 
-print("Original Message is: ", message)
-c = encrypt(message)
-print("Encrypted message is: ", c);
-m = decrypt(c);
-print("Decrypted message is: ", m);
+
+def gcd(a, b):
+    if(b == 0):
+        return a
+    else:
+        return gcd(b, a % b)
+     
+
+p = generatePrime(2048)
+q = generatePrime(2048)
+e = generatePubKey(p, q)
+d = calculatePrivKey(p, q, e)
+print("Public key is ", e)
+print("Private key is ", d)
+     
+#message = int(input("Enter the message to be encrypted: ")) 
+
+#print("Original Message is: ", message)
+#c = encrypt(message)
+#print("Encrypted message is: ", c)
+#m = decrypt(c);
+#print("Decrypted message is: ", m)
