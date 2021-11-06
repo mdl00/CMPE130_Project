9e344266ca4416c78a6d7281e254e51259d6b5b8
Brandon <35442266+RedRito@users.noreply.github.com>
2021-11-05 19:45:02 -0700

    Updated to current chat client.
    Work that needs to be done:
    -Encryption on the messages and names
    -Clean up
    -Real comments and not ones from the document.
    -Analysis (runtime) using the "timeit" library

--- chatProgramClient.py
+++ chatProgramClient.py
@@ -1,56 +1,91 @@
 import socket
-#import select
 import errno
 import sys
-
-#from chatProgram import HEADER_LENGTH
-
-#documentation is found at https://pythonprogramming.net/client-chatroom-sockets-tutorial-python-3/
+import timeit
 
 HEADER_LENGTH = 10
-IP = "10.0.0.179"
-PORT = 4567
-my_username = input("Username: ")
+PRIVATEKEY = 10
 
+IP = "127.0.0.1"
+PORT = 1234
+my_username = input("Enter a Username: ")
+
+# Create a socket
+# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
+# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
 client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-client_socket.connect((IP, PORT))
-client_socket.setblocking(False)
 
-username = my_username.encode("utf-8")
-username_header = f"{len(username):<{HEADER_LENGTH}}".encode("utf-8")
+# Connect to a given ip and port
+client_socket.connect((IP, PORT))
+
+# Set connection to non-blocking state, so .recv() call won't block, just return some exception we'll handle
+client_socket.setblocking(True)
+
+# Prepare username and header and send them
+# We need to encode username to bytes, then count number of bytes and prepare header of fixed size, that we encode to bytes as well
+username = my_username.encode('utf-8')
+username_header = f"{len(username):<{HEADER_LENGTH}}".encode('utf-8')
 client_socket.send(username_header + username)
 
+#PRIV KEY HANDOUT
+KEYHEADER = client_socket.recv(PRIVATEKEY)
+KEYLENGTH = int(KEYHEADER.decode('utf-8').strip())
+KEY = client_socket.recv(KEYLENGTH).decode('utf-8')
+print(KEYHEADER)    #JUST DEBUGGING
+print(KEY)          #JUST DEBUGGING
+#note: it got to work, just needs to be able to encrypt and send using this key now
 while True:
-    message = input(f"{my_username} > ")
-    message = ""
+
+    # Wait for user to input a message
+    message = input(f'{my_username} > ')
+
+    # If message is not empty - send it
     if message:
-        message = message.encode("utf-8")
-        message_header = f"{len(message) :<{HEADER_LENGTH}}".encode("utf-8")
+
+        # Encode message to bytes, prepare header and convert to bytes, like for username above, then send
+        message = message.encode('utf-8')
+        message_header = f"{len(message):<{HEADER_LENGTH}}".encode('utf-8')
         client_socket.send(message_header + message)
-    
+
     try:
+        # Now we want to loop over received messages (there might be more than one) and print them
         while True:
-            #receive messages until error
+
+            # Receive our "header" containing username length, it's size is defined and constant
             username_header = client_socket.recv(HEADER_LENGTH)
+
+            # If we received no data, server gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
             if not len(username_header):
-                print("connect closed by the server")
+                print('Connection closed by the server')
                 sys.exit()
 
-            username_length = int(username_header.decode("utf-8").strip())
-            username = client_socket.recv(username_length).decode("utf-8")
+            # Convert header to int value
+            username_length = int(username_header.decode('utf-8').strip())
 
+            # Receive and decode username
+            username = client_socket.recv(username_length).decode('utf-8')
+
+            # Now do the same for message (as we received username, we received whole message, there's no need to check if it has any length)
             message_header = client_socket.recv(HEADER_LENGTH)
-            message_length = int(message_header.decode("utf-8".strip()))
-            message = client_socket.recv(message_length).decode("utf-8")
+            message_length = int(message_header.decode('utf-8').strip())
+            message = client_socket.recv(message_length).decode('utf-8')
 
-            print(f"{username} > {message}")
+            # Print message
+            print(f'{username} > {message}')
 
     except IOError as e:
+        # This is normal on non blocking connections - when there are no incoming data error is going to be raised
+        # Some operating systems will indicate that using AGAIN, and some using WOULDBLOCK error code
+        # We are going to check for both - if one of them - that's expected, means no incoming data, continue as normal
+        # If we got different error code - something happened
         if e.errno != errno.EAGAIN and e.errno != errno.EWOULDBLOCK:
-            print('Readin error', str(e))
+            print('Reading error: {}'.format(str(e)))
             sys.exit()
+
+        # We just did not receive anything
         continue
 
     except Exception as e:
-        print('Error!',str(e))
+        # Any other exception - something happened, exit
+        print('Reading error: '.format(str(e)))
         sys.exit()
