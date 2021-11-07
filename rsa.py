a43a863c5a93cc50102f45ff1331324f7055e08e
Brandon <35442266+RedRito@users.noreply.github.com>
2021-11-06 20:50:17 -0700

    Update rsa.py

--- rsa.py
+++ rsa.py
@@ -13,6 +13,7 @@ low_primes = [2, 3, 5, 7, 11, 13, 17, 19, 23, 29,
                      353, 359, 367, 373, 379, 383, 389,	397,
                      401, 409]
  
+#returns a random number generated fom 2 ^(n-1) + 1 and 2 ^ n - 1
 def n_bit_random(n):
     return random.randrange(2**(n-1)+1, 2**n - 1)
  
@@ -52,18 +53,29 @@ def is_rabin_miller(mrc):
             return False
     return True
  
+#generates a prime number given a certain number of bits
+#It will create a random value relative to the given bits and run that value through a primality test
+#returns a value that passes through the rabin miller primality test
 def generate_prime(n):
- while True:
-     prime_candidate = get_low_prime(n)
-     if not is_rabin_miller(prime_candidate):
+ while True:                                    #loop forever until a prime number is found
+     prime_candidate = get_low_prime(n)         #Random Value
+     if not is_rabin_miller(prime_candidate):     
          continue
      else:
-         return prime_candidate
+         return prime_candidate                 #If its prime, return the prime value
 
+
+#encrypts a message given the message represented in an int value, n (p*q), and the public key
+# m^e mod n
+#returns the encrypted message
 def encrypt(m, n, e):
     c = int((m**e) % n)
     return c
 
+#decrypts a given message represented as a integer
+# c^d mod n (typical RSA decryption formula)
+#c is the message, d is the private key, n is the result of p * q
+#returns the decrypted message
 def decrypt(c, n, d):
     m = pow(c, d, n)
     return m
@@ -90,12 +102,12 @@ def get_public_key():
 
 #p and q are calculated during the creation of the server and can be changed when needed. (session key)
 def create_private_key(p, q):
-    
     e = get_public_key()
     z = (q-1)*(p-1)
-    gcd, x, b = extended_euclidean_algorithm(e, z)
-    d = x
-    d = d % z
-    if(d < 0):
+    gcd, x, b = extended_euclidean_algorithm(e, z)  #gets the greatest common demoninator, the x from the equation(AKA the private key), and the remaning y from the equation.
+    d = x                                          
+    d = d % z                                       #This is for when D is negative
+                                                    #-D = -D + k * phi #equation from a+k⋅m
+    if(d < 0):                                      #Simply just increment k so that D becomes positive            
         d += z
     return d
