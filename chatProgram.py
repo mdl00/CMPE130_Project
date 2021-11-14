1389e284c47f5a6473834eeb06711482b9b9a746
Brandon <35442266+RedRito@users.noreply.github.com>
2021-11-13 20:04:34 -0800

    Update chatProgram.py

--- chatProgram.py
+++ chatProgram.py
@@ -1,94 +1,85 @@
 import socket
 import select
 import rsa
-
+import time
 #CREATE KEYS
-p = rsa.generate_prime(2048)
-q = rsa.generate_prime(2048)
+start_time = time.perf_counter()            #this is for keeping track of runtime
+p = rsa.generate_prime(1024)
+q = rsa.generate_prime(1024)
 n = p * q
-PUBLICKEY = rsa.get_public_key()
-PRIVATEKEY = rsa.create_private_key(p,q)
+
+PUBLIC_KEY = rsa.get_public_key()            #predefined constant
+PRIVATE_KEY = rsa.create_private_key(p,q)    #create private key given p and q
+
+end_time = time.perf_counter()
+
+total_time = (end_time - start_time)
+
 #length of messages
 HEADER_LENGTH = 10
 
-IP = "127.0.0.1"
-PORT = 1234
+IP = "127.0.0.1"                            #our server address *note this is a local address.
+PORT = 1234                                 #what port our server is using
 
-# Create a socket
-# socket.AF_INET - address family, IPv4, some otehr possible are AF_INET6, AF_BLUETOOTH, AF_UNIX
-# socket.SOCK_STREAM - TCP, conection-based, socket.SOCK_DGRAM - UDP, connectionless, datagrams, socket.SOCK_RAW - raw IP packets
+#Create a socket
 server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-# SO_ - socket option
-# SOL_ - socket option level
-# Sets REUSEADDR (as a socket option) to 1 on socket
+#socket options. This allows us to reuse the same address in case some ports are filled up we can keep using the same one.
 server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 
-# Bind, so server informs operating system that it's going to use given IP and port
-# For a server using 0.0.0.0 means to listen on all available interfaces, useful to connect locally to 127.0.0.1 and remotely to LAN interface IP
+#Bind, so server informs operating system that it's going to use given IP and port
 server_socket.bind((IP, PORT))
 
-# This makes server listen to new connections
+#Look for connections to the server
 server_socket.listen()
 
-# List of sockets for select.select()
+#List of sockets
 sockets_list = [server_socket]
 
-# List of connected clients - socket as a key, user header and name as data
+#List of connected clients - socket as a key, user header and name as data
 clients = {}
+
 print('New server created')
 print('New private key created')
 print(f'Listening for connections on {IP}:{PORT}...')
 
-# Handles message receiving
+#If message is recieved this keeps track of it and displays it to the user
 def receive_message(client_socket):
 
     try:
 
-        # Receive our "header" containing message length, it's size is defined and constant
+        #Recieve message length
         message_header = client_socket.recv(HEADER_LENGTH)
 
-        # If we received no data, client gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+        #Error handling, if nothing is sent return no message receieved
         if not len(message_header):
             return False
 
-        # Convert header to int value
+        #Convert message length back to int value after decoding from unicode
         message_length = int(message_header.decode('utf-8').strip())
 
-        # Return an object of message header and message data
+        #Return an object of message header and message data
         return {'header': message_header, 'data': client_socket.recv(message_length)}
 
+    #eception handling, if we did not recieve a message correctly
     except:
-
-        # If we are here, client closed connection violently, for example by pressing ctrl+c on his script
-        # or just lost his connection
-        # socket.close() also invokes socket.shutdown(socket.SHUT_RDWR) what sends information about closing the socket (shutdown read/write)
-        # and that's also a cause when we receive an empty message
         return False
 
 while True:
 
-    # Calls Unix select() system call or Windows select() WinSock call with three parameters:
-    #   - rlist - sockets to be monitored for incoming data
-    #   - wlist - sockets for data to be send to (checks if for example buffers are not full and socket is ready to send some data)
-    #   - xlist - sockets to be monitored for exceptions (we want to monitor all sockets for errors, so we can use rlist)
-    # Returns lists:
-    #   - reading - sockets we received some data on (that way we don't have to check sockets manually)
-    #   - writing - sockets ready for data to be send thru them
-    #   - errors  - sockets with some exceptions
-    # This is a blocking call, code execution will "wait" here and "get" notified in case any action should be taken
-    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+    #select.select has 3 parameters(rlist, wlist, xlist)
+    #it returns 3 lists: reading list, writing list, error list
+    read_sockets, temp, exception_sockets = select.select(sockets_list, [], sockets_list)   #writing list is unused thus is labeled temp
 
 
-    # Iterate over notified sockets
+    #Iterate over notified sockets
     for notified_socket in read_sockets:
 
-        # If notified socket is a server socket - new connection, accept it
+        # If notified socket is a server socket it is a new connection
         if notified_socket == server_socket:
 
             # Accept new connection
-            # That gives us new socket - client socket, connected to this given client only, it's unique for that client
-            # The other returned object is ip/port set
+            #returns the client socket and its address
             client_socket, client_address = server_socket.accept()
 
             # Client should send his name right away, receive it
@@ -106,13 +97,18 @@ while True:
 
             print('NEW CLIENT CONNECTED: {}:{}, username: {}'.format(*client_address, user['data'].decode('utf-8')))
             # HELP!!!!!!!!!!!!!!!!!!!!! Need to send key over to connected client. However, the send function returns an byte of b'' even though the privKey is b'(some value for key)'
-            privKey = str(PRIVATEKEY).encode('utf-8')
+            privKey = str(PRIVATE_KEY).encode('utf-8')
             keyHeader = f"{len(privKey):<{HEADER_LENGTH}}".encode('utf-8')
-            pubKey = str(PUBLICKEY).encode('utf-8')
-            print(privKey)
+
             client_socket.send(keyHeader + privKey)
+
+            pubKey = str(PUBLIC_KEY).encode('utf-8')
             keyHeader = f"{len(pubKey):<{HEADER_LENGTH}}".encode('utf-8')
-            client_socket.send(pubKey)
+            client_socket.send(keyHeader + pubKey)
+
+            modN = str(n).encode('utf-8')
+            n_header = f"{len(modN):<{HEADER_LENGTH}}".encode('utf-8')
+            client_socket.send(n_header + modN)
             #!!!!!!!!!!!!!!!!!!!!!!!!!!!
             #note it works now.
 
